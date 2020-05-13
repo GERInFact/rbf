8e33ef2c455e6e9e5e2384d153199a8a61975b93
M. Hille <marciausgoslar@gmail.com>
2020-05-13 09:50:16 +0200

    Updates

---
+++ precache-manifest.6d2a30196b5b2c051bb981a462fcfbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37dff53ae3e83885a53373bf2cf1d66c",
+    "url": "/GERInFact/rbf.git/index.html"
+  },
+  {
+    "revision": "39cba5adde046c190208",
+    "url": "/GERInFact/rbf.git/static/css/main.ee10b6d9.chunk.css"
+  },
+  {
+    "revision": "db87e3a0e377c7250531",
+    "url": "/GERInFact/rbf.git/static/js/2.e15f78d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GERInFact/rbf.git/static/js/2.e15f78d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39cba5adde046c190208",
+    "url": "/GERInFact/rbf.git/static/js/main.39234ed8.chunk.js"
+  },
+  {
+    "revision": "6a78a970e1338c3b8ae5",
+    "url": "/GERInFact/rbf.git/static/js/runtime-main.e3072440.js"
+  }
+]);
